397e9b895964dca1d54fe02f4ac4422e8742b73d
Chris Closser <chrisclosser1@gmail.com>
2017-12-26 11:25:17 -0800

    added main block for testing

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -17,3 +17,25 @@ def quick_sort(vals):
             right.append(val)
 
     return quick_sort(left) + [axis] + quick_sort(right)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from timeit import timeit
+    from random import randint
+    setup = 'from quick_sort import quick_sort'
+
+    print('>>> best')
+    for length in range(5, 20, 5):
+        i = [randint(-100, 100) for _ in range(length)]
+
+        print('Input: unsorted {}'.format(i))
+        time = timeit('quick_sort({})'.format(i), setup)
+        print('    average time: {}ms\n'.format(time))
+
+    print('<<< worst')
+    for length in range(5, 20, 5):
+        i = sorted([randint(-100, 100) for _ in range(length)])
+
+        print('Input: sorted {}'.format(i))
+        time = timeit('quick_sort({})'.format(i), setup)
+        print('    average time: {}ms\n'.format(time))
